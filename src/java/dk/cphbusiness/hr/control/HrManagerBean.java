ed44b8339aec5fa83dbe9eedecf16ee816bb6c82
Anders <Anders@10.18.138.24>
2014-03-17 11:02:44 +0100

    Initial commit

---
+++ src/java/dk/cphbusiness/hr/control/HrManagerBean.java
@@ -0,0 +1,16 @@
+
+package dk.cphbusiness.hr.control;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Anders
+ */
+@Stateless
+public class HrManagerBean implements HrManagerBeanRemote
+{
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
